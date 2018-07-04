1cf197257db18d3e18b7a7dac9df15d270964f60
chongerls <474988135@qq.com>
2018-07-04 14:23:48 +0800

    Initial commit

---
+++ src/com/ls/hello.java
@@ -0,0 +1,12 @@
+package com.ls;
+
+/**
+ * Create By lishan10 on 2018/7/1
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("@@@@@@@@@@@@Hello World!ß~~~~~~~~~~~~~~ß");
+    }
+
+}
